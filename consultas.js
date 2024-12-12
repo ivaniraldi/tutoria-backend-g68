a3f0afc2213d2eb972771b6f0787801c071549be
ivaniraldi <iraldiban@gmail.com>
2024-12-11 23:10:02 -0300

    parte 2

--- consultas.js
+++ consultas.js
@@ -1,7 +1,6 @@
 const pool = require('./db');
 
-const agregarCliente = async (req, res) =>{
-    const {email, nombre} = req.body;
+const agregarCliente = async (email, nombre) =>{
     const consulta = `INSERT INTO clientes (email, nombre) VALUES ($1, $2)`;
     const values = [nombre, email]
     const result = await pool.query(consulta, values);
@@ -10,11 +9,37 @@ const agregarCliente = async (req, res) =>{
 const obtenerClientes = async(req, res)=>{
     const consulta = "SELECT * FROM clientes";
     const result = await pool.query(consulta);
-    res.json(result.rows);
+    if(result.rowCount == 0){
+        throw { code: 404, message: "No hay clientes en la base de datos"}
+    }
+    return result
+}
+
+const modificarCliente = async (email, nombre, id) =>{
+
+    const consulta = `UPDATE clientes SET email = $1, nombre = $2 WHERE id = $3`;
+    const values = [email, nombre, id]
+    const result = await pool.query(consulta, values);
+    if(result.rowCount == 0){
+        throw { code: 404, message: "No se encontro el cliente con ese id"}
+    }
+    console.log(result)
+    
+}
+
+const eliminarCliente = async (id) =>{
+    const consulta = `DELETE FROM clientes WHERE id = $1`;
+    const result = await pool.query(consulta,[id]);
+    if(result.rowCount == 0){
+        throw { code: 404, message: "No se encontro el cliente con ese id"}
+    }
+
 }
     
 
 module.exports = {
     agregarCliente,
-    obtenerClientes
+    obtenerClientes,
+    modificarCliente,
+    eliminarCliente
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('./db');
 const cors = require('cors');
-const { agregarCliente, obtenerClientes } = require('./consultas');
+const { agregarCliente, obtenerClientes, modificarCliente, eliminarCliente } = require('./consultas');
 
 const app = express();
 
@@ -10,14 +10,60 @@ app.use(cors());
 
 app.listen(3000, ()=>{console.log('Server started')});
 
-app.get("/clientes", obtenerClientes)
+app.get("/clientes", async (req, res) =>{    
+    try {
+        const result = await obtenerClientes();
+        res.json(result.rows);
+    } catch (error) {
+        res.status(error.code).send(error.message)
+    }
+})
 
 app.get("/clientes/:id", async (req,res) =>{
     const {id} = req.params;
     const consulta = `SELECT * FROM clientes WHERE id = $1`;
     const values = [id]
     const result = await pool.query(consulta, values);
-    res.json(result.rows);
+    if(result.rowCount == 0){
+        res.status(404).send("No se encontro el cliente con ese id")
+    }else{
+        res.json(result.rows);
+    }
 })
 
-app.post("/clientes", agregarCliente)
+app.post("/clientes", async (req, res) =>{
+    const {email, nombre} = req.body;
+    try {
+        await agregarCliente(email, nombre);
+        res.send("Cliente creado con exito!")
+    } catch (error) {
+        if (error.code == "23502"){
+            res.status(500).send("Debes completar todos los campos")
+        }else{
+            res.status(500).send("Error al crear cliente")
+        }
+    }
+})
+
+app.put("/clientes/:id", async (req, res) =>{
+ const {id} = req.params;
+ const {email, nombre} = req.body;
+ try {
+    await modificarCliente(email, nombre, id);
+    res.send("Cliente modificado con exito!")
+ } catch (error) {
+     res.status(error.code).send(error.message)
+    
+ }
+})
+
+app.delete("/clientes/:id", async (req, res) =>{
+    const {id} = req.params;
+   try {
+    await eliminarCliente(id);
+    res.send("Cliente eliminado con exito!")
+   } catch (error) {
+       res.status(error.code).send(error.message)
+   }
+
+})
